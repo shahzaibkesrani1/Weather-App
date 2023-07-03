23ec87cb4747762904ab6cc2bfe9e9db5be74baf
ShahzaibKesrani <111377043+shahzaibkesrani1@users.noreply.github.com>
2023-07-04 02:42:17 +0500

    Add files via upload

---
+++ sk.js
@@ -0,0 +1,62 @@
+const inputBox = document.querySelector('.input-b');
+const searchBtn = document.getElementById('btn');
+const weather_img = document.querySelector('.Weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind');
+
+const location_not_found = document.querySelector('.location');
+
+const weather_body = document.querySelector('.weather-body');
+
+
+async function checkWeather(city){
+    const api_key = "dd95b5a7a3f07396a92859ebc3552fa3";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        console.log("error");
+        return;
+    }
+
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+
+    switch(weather_data.weather[0].main){
+        case 'Clouds':
+            weather_img.src = "images/cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "images/clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "images/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "images/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "images/snow.png";
+            break;
+
+    }
+
+    console.log(weather_data);
+}
+
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
